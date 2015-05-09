8ef582f4367a9ad4ebc1669dc543cdd9984257cc
Varun R Sekar <rapidwein@gmail.com>
2015-05-09 11:12:28 +0530

    template - basic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var swig = require("swig");
+swig.renderFile("src/templates/app.html");
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
                         response.end();
