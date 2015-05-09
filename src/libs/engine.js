f33e6cf98f5ce4b1b1b0ad756ecc7eec547e82df
Prasanna <prasanmouli@gmail.com>
2015-05-10 03:22:36 +0530

    drag and position added

--- src/libs/engine.js
+++ src/libs/engine.js
@@ -3,36 +3,55 @@
 // Matter.js
 var ENGINE = Matter.Engine,
     WORLD = Matter.World,
-    BODY = Matter.Body,
+    B = Matter.Body,
+    RENDER = Matter.Render,
     BODIES = Matter.Bodies,
-    engine,
-    MouseConstraint = Matter.MouseConstraint;
-
+    EVENTS = Matter.Events;
 // Canvas Editor Studio
+var STUDIO = document.getElementById('studio'),
+    MouseConstraint = Matter.MouseConstraint,
+    MatterConstraint = Matter.Constraint,
+    mouse_constraint;
+
     
 /* Common Matter.js Functionalities */
+module.exports.addBody = function (options){
+    var chamfer = options['chamfer'];
+    options['chamfer'] = {'radius': chamfer};
+    options['render'] = {
+	fillStyle: '#6B3434',
+        strokeStyle: '#6B3434',
+	lineWidth: 3
+    };
+    options['isStatic'] = false;
 
-module.exports.addBody = function (){
-    var polygon = BODIES.polygon(100, 200, 5, 100, { render: { lineWidth: 5, strokeStyle: '#ffffff'} });
+    var polygon = BODIES.polygon(400, 300, options['sides'], options['size'], options);
     WORLD.addBody(engine.world, polygon);
+    
+    EVENTS.on(polygon, 'startdrag', function(event) {
+        console.log('tick22', event);
+    })
+    EVENTS.trigger(polygon, 'startdrag', function(event) {
+        console.log('tick11', event);
+    })
+    
 };
 
 module.exports.runEngine = function (){
     ENGINE.run(engine);
-    Matter.Render.world(engine)
 };
 
-
 module.exports.init = function(){
-    var STUDIO = document.getElementById('studio'),
-    opt = {
-            positionIterations: 6,
-            velocityIterations: 4,
-            enableSleeping: false
+    var options = {
+	timeScale: 1,
+        positionIterations: 6,
+        velocityIterations: 4,
+        enableSleeping: false
     };
-    engine = ENGINE.create(STUDIO);
-    var mouse_constraint = MouseConstraint.create(engine);
+    engine = ENGINE.create(STUDIO, options);
+    mouse_constraint = MouseConstraint.create(engine);
     WORLD.add(engine.world,mouse_constraint);
+    engine.world.gravity.y = 0;
     ENGINE.run(engine);
 }
 
@@ -42,6 +61,7 @@ module.exports.globals = function(){
     world : WORLD,
     body : BODY,
     bodies : BODIES,
+    render : RENDER,
     studio : STUDIO
     }
 }
--- src/static/local.js
+++ src/static/local.js
@@ -1,55 +1,73 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports.sketchUp = function(canvasObj){
-  canvasObj.sketch({ defaultColor : "#ff0", defaultTool : "marker", defaultSize : 5});
-}
-module.exports.erase = function(event){
-
-}
-
-},{}],2:[function(require,module,exports){
 /* Aliases and Globals */
 
 // Matter.js
 var ENGINE = Matter.Engine,
     WORLD = Matter.World,
-    BODY = Matter.Body,
-    BODIES = Matter.Bodies;
+    B = Matter.Body,
+    RENDER = Matter.Render,
+    BODIES = Matter.Bodies,
+    EVENTS = Matter.Events;
 // Canvas Editor Studio
 var STUDIO = document.getElementById('studio'),
-    engine = ENGINE.create(STUDIO);
+    MouseConstraint = Matter.MouseConstraint,
+    MatterConstraint = Matter.Constraint,
+    mouse_constraint;
 
-
+    
 /* Common Matter.js Functionalities */
-function addBody(options){
-    console.log(options);
-//    var polygon = BODIES.polygon(100, 200, options['sides'], options['radius'], options['size']);
-    var polygon = BODIES.polygon(100, 200, 5, 10, { isStatic: true, render: { lineWidth: 5, strokeStyle: '#000000'} });
-    console.log(BODY);
-    //BODY.setVelocity(polygon, {x:0, y:0});
-    console.log(polygon, engine, engine.world);
+module.exports.addBody = function (options){
+    var chamfer = options['chamfer'];
+    options['chamfer'] = {'radius': chamfer};
+    options['render'] = {
+	fillStyle: '#6B3434',
+        strokeStyle: '#6B3434',
+	lineWidth: 3
+    };
+    options['isStatic'] = false;
+
+    var polygon = BODIES.polygon(400, 300, options['sides'], options['size'], options);
     WORLD.addBody(engine.world, polygon);
-}
+    
+    EVENTS.on(polygon, 'startdrag', function(event) {
+        console.log('tick22', event);
+    })
+    EVENTS.trigger(polygon, 'startdrag', function(event) {
+        console.log('tick11', event);
+    })
+    
+};
 
-function runEngine(){
-    console.log(engine);
+module.exports.runEngine = function (){
     ENGINE.run(engine);
-    console.log(engine);
-    Matter.Render.world(engine)
+};
+
+module.exports.init = function(){
+    var options = {
+	timeScale: 1,
+        positionIterations: 6,
+        velocityIterations: 4,
+        enableSleeping: false
+    };
+    engine = ENGINE.create(STUDIO, options);
+    mouse_constraint = MouseConstraint.create(engine);
+    WORLD.add(engine.world,mouse_constraint);
+    engine.world.gravity.y = 0;
+    ENGINE.run(engine);
 }
 
-module.exports.addBody = addBody;
-module.exports.runEngine = runEngine;
 module.exports.globals = function(){
     return {
-	engine : ENGINE,
-	world : WORLD,
-	body : BODY,
-	bodies : BODIES,
-	studio : STUDIO
+    engine : ENGINE,
+    world : WORLD,
+    body : BODY,
+    bodies : BODIES,
+    render : RENDER,
+    studio : STUDIO
     }
 }
 
-},{}],3:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 var studio_engine = require("./engine.js");
 
 var toolbox = $('#tool-div .section');
@@ -66,18 +84,18 @@ toolbox.on("click", function(event){
     }
 });
 
-module.exports.runner = function(n){
-  return n;
-}
 
 /* Matter Events */
 
-},{"./engine.js":2}],4:[function(require,module,exports){
+},{"./engine.js":1}],3:[function(require,module,exports){
 var events = require("./events.js");
-var editor = require("./editor.js");
+//var editor = require("./editor.js");
+var engine = require('./engine.js');
 
 var canvasObj = $('#studio');
-editor.sketchUp(canvasObj);
+//editor.sketchUp(canvasObj);
+engine.init();
 
+var mode=$('.mode').attr('id');
 
-},{"./editor.js":1,"./events.js":3}]},{},[4]);
+},{"./engine.js":1,"./events.js":2}]},{},[3]);
