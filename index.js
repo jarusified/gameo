85a9b3a33b3fbd76e3e08f4635a8b3e503eb6c6d
Prasanna <prasanmouli@gmail.com>
2015-05-09 13:08:41 +0530

    remove db.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var http = require("http"),
     swig = require("swig"),
-    mongoose = require('mongoose'),
-    db = require('./src/static/db.js');
+    mongoose = require('mongoose');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write(swig.renderFile('./src/templates/app.html'));
