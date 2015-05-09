4ddfd0e39a4b6d77ec7a8d0d1ddad66e4dbfdfe6
Varun <rapidwein@gmail.com>
2015-05-09 18:47:16 +0530

    added data to globals db

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ var http = require("http"),
     express = require('express'),
     mongoose = require('mongoose'),
     app=express(),
-    routes = require('./routes');
+    routes = require('./routes'),
+    schema = require('./src/static/schema.js');
 
 app.engine('html',swig.renderFile);
 app.set('view engine','html');
 app.set('views',__dirname+'/src/templates');
-app.use('/src/static',express.static(__dirname+'/src/static'));
-
+app.use(express.static(__dirname+'/src/static'));
+mongoose.connect("mongodb://localhost:27017/gameo");
+var objectT = {1:"character",2:"background",3:"platform",4:"obstacle",5:"others",6:"world"},
+    controlT = {1:"jump",2:"left",3:"right",4:"duck",5:"shoot"};
+var example = new schema.Globals({object_types : objectT ,control_types : controlT});
+example.save(function(err){
+  if(err){
+    return handleError(err);
+  }
+});
+schema.Globals.find({},function(err,data){
+  if(err){
+    return handleError(err);
+  }
+  console.log(data);
+});
 routes.init(app);
 
 app.listen(8080);
--- src/static/schema.js
+++ src/static/schema.js
@@ -1,31 +1,35 @@
 var exports = module.exports;
 var mongoose = require("mongoose");
-exports.wrapper = function(){
-  var usersSchema = new mongoose.Schema({
-    id : { type : Number},
-    name : { type : String},
-  });
-  var gamesSchema = new mongoose.Schema({
-    id : { type : Number},
-    user_id : { type : Number},
-    type : { type : Number},
-    desc : { type : String}
-  });
-  var globalsSchema = new mongoose.Schema({
-    object_types : { type : Types.Mixed},
-    control_type : { type : Types.Mixed}
-  });
-  var objectsSchema = new mongoose.Schema({
-    id : { type : Number},
-    game_id : { type : Number},
-    states : { type : Types.Mixed}
-  });
-  var controlsSchema = new mongoose.Schema({
-    game_id : { type : Number},
-    controls : { type : Types.Mixed}
-  });
-  var Users = mongoose.model("Users", usersSchema);
-  var Games = mongoose.model("Games", usersSchema);
-  var Globals = mongoose.model("Globals", usersSchema);
-  var Objects = mongoose.model("Objects", usersSchema);
-}
+var usersSchema = new mongoose.Schema({
+  id : { type : Number},
+  name : { type : String},
+});
+var gamesSchema = new mongoose.Schema({
+  id : { type : Number},
+  user_id : { type : Number},
+  type : { type : Number},
+  desc : { type : String}
+});
+var globalsSchema = new mongoose.Schema({
+  object_types : { type : mongoose.Schema.Types.Mixed},
+  control_types : { type : mongoose.Schema.Types.Mixed}
+});
+var objectsSchema = new mongoose.Schema({
+  id : { type : Number},
+  game_id : { type : Number},
+  states : { type : mongoose.Schema.Types.Mixed}
+});
+var controlsSchema = new mongoose.Schema({
+  game_id : { type : Number},
+  controls : { type : mongoose.Schema.Types.Mixed}
+});
+var Users = mongoose.model("Users", usersSchema),
+    Games = mongoose.model("Games", gamesSchema),
+    Globals = mongoose.model("Globals", globalsSchema),
+    Objects = mongoose.model("Objects", objectsSchema),
+    Controls = mongoose.model("Controls", controlsSchema);
+exports.Users = Users;
+exports.Games = Games;
+exports.Globals = Globals;
+exports.Objects = Objects;
+exports.Controls = Controls;
