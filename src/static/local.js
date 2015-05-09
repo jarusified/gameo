266501a2a63d839d19f3212095e5d933faeb1f8f
Prasanna <prasanmouli@gmail.com>
2015-05-09 19:51:06 +0530

    libraries are now working

--- src/static/local.js
+++ src/static/local.js
@@ -1,10 +1,40 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var TOOLBOX = document.getElementById('tool-div');
 
+/* User Events */
+TOOLBOX.addEventListener("click", function(event){
+  console.log('AAA');
+});
+module.exports.runner = function(n){
+  return n;
+}
+
+/* Matter Events */
 
 },{}],2:[function(require,module,exports){
-arguments[4][1][0].apply(exports,arguments)
-},{"dup":1}],3:[function(require,module,exports){
-arguments[4][1][0].apply(exports,arguments)
-},{"dup":1}],4:[function(require,module,exports){
-arguments[4][1][0].apply(exports,arguments)
-},{"dup":1}]},{},[1,2,3,4]);
+var temp = require("./events.js");
+console.log(temp.runner(3));
+
+/* Aliases and Globals */
+// Matter.js
+var ENGINE = Matter.Engine,
+    WORLD = Matter.World,
+    BODIES = Matter.Bodies;
+// Canvas Editor Studio
+var STUDIO = document.getElementById('studio');
+
+/* Common Matter.js Functionalities */
+function initEngine(){
+    var engine = ENGINE.create(STUDIO);
+    return engine;
+}
+
+function addBody(engine, bodies){
+    WORLD.add(engine.world, bodies);
+}
+
+function runEngine(engine){
+    ENGINE.run(engine);
+}
+
+},{"./events.js":1}]},{},[2]);
